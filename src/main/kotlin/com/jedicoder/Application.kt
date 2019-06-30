2b900acec585e731a6be0f9c374456899a79da8d
aslivets <slivets.aleksay@gmail.com>
2019-06-30 14:31:39 +0200

    add base security routes

--- src/main/kotlin/com/jedicoder/Application.kt
+++ src/main/kotlin/com/jedicoder/Application.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
 class Application
+    fun main(args: Array<String>) {
+        SpringApplication.run(Application::class.java, *args)
+    }
 
-fun main(args: Array<String>) {
-    SpringApplication.run(Application::class.java, *args)
-}
